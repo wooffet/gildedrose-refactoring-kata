230824b72ba6937962002eb535d3a69b51e06320
Chris Nightingale <wooffet@gmail.com>
2025-02-05 16:42:07 +0000

    Initial refactor of UpdateQuality function now unit tests are in place

--- csharp.NUnit/GildedRose/GildedRose.cs
+++ csharp.NUnit/GildedRose/GildedRose.cs
@@ -10,7 +10,7 @@ public class GildedRose
     private readonly int MAX_NON_LEGENDARY_QUALITY = 50;
     private readonly int MIN_NON_LEGENDARY_QUALITY = 0;
     private readonly string SPECIALITEM_AGED_ITEM_PREFIX = "Aged";
-    private readonly string SPECIALITEM_BACKSTAGE_PASS_PREFIX = "Backstage pass"; // double check backstage items will always have this prefix with PM/BA
+    private readonly string SPECIALITEM_BACKSTAGE_PASS_ITEM_PREFIX = "Backstage pass"; // double check backstage items will always have this prefix with PM/BA
     private readonly string SPECIALITEM_CONJURED_ITEM_NAME_PREFIX = "Conjured";
 
     public GildedRose(IList<Item> Items)
@@ -18,105 +18,79 @@ public class GildedRose
         this.Items = Items;
     }
 
+    private bool CanUpdateItem(int itemQuality) => IsNonLegendaryQualityItem(itemQuality) && itemQuality > MIN_NON_LEGENDARY_QUALITY;
+    private bool IsNonLegendaryQualityItem(int itemQuality) => itemQuality < LEGENDARY_QUALITY;
+    private bool IsSpecialItem(string itemName) => itemName.StartsWith(SPECIALITEM_AGED_ITEM_PREFIX) || 
+        itemName.StartsWith(SPECIALITEM_BACKSTAGE_PASS_ITEM_PREFIX) || itemName.StartsWith(SPECIALITEM_CONJURED_ITEM_NAME_PREFIX);
+
     public void UpdateQuality()
     {
         for (var i = 0; i < Items.Count; i++)
         {
             var currentItem = Items[i];
 
-            // if normal item
-            if (!currentItem.Name.StartsWith(SPECIALITEM_AGED_ITEM_PREFIX) && !currentItem.Name.StartsWith(SPECIALITEM_BACKSTAGE_PASS_PREFIX)
-                && !currentItem.Name.StartsWith(SPECIALITEM_CONJURED_ITEM_NAME_PREFIX))
+            // check if we can process the item (quality > 0 and not a legendary quality item)
+            // we can avoid processing items that are already minimum quality (quality cannot be less than 0) or are legendary quality (sellin/quality never changes)
+            if (CanUpdateItem(currentItem.Quality))
             {
-                if (currentItem.Quality > MIN_NON_LEGENDARY_QUALITY)
+                // reduce the SellIn days by 1
+                currentItem.SellIn -= 1;
+
+                // if special item (aged, backstage pass or conjured)
+                if (IsSpecialItem(currentItem.Name))
                 {
-                    if (currentItem.Quality != LEGENDARY_QUALITY)
+                    if (currentItem.Name.StartsWith(SPECIALITEM_AGED_ITEM_PREFIX))
                     {
-                        currentItem.Quality = currentItem.Quality - 1;
+                        currentItem.Quality += 1;
                     }
-                }
-            }
-            // else if brie, backstage pass or conjured
-            else
-            {
-                if (currentItem.Quality < MAX_NON_LEGENDARY_QUALITY)
-                {
+
+                    if (currentItem.Name.StartsWith(SPECIALITEM_BACKSTAGE_PASS_ITEM_PREFIX))
+                    {
+                        if (currentItem.SellIn > 10)
+                        {
+                            currentItem.Quality += 1;
+                        }
+
+                        if (currentItem.SellIn <= 10 && currentItem.SellIn > 5) 
+                        {
+                            currentItem.Quality += 2;
+                        }
+
+                        if (currentItem.SellIn <= 5)
+                        {
+                            currentItem.Quality += 3;
+                        }
+
+                        if (currentItem.SellIn < 0)
+                        {
+                            currentItem.Quality = 0;
+                        }
+                    }
+
                     if (currentItem.Name.StartsWith(SPECIALITEM_CONJURED_ITEM_NAME_PREFIX))
                     {
-                        if (currentItem.SellIn > 0)
-                        {
-                            currentItem.Quality -= 2;
-                        }
-                    }
-                    else
-                    {
-                        currentItem.Quality = currentItem.Quality + 1;
-
-                        if (currentItem.Name.StartsWith(SPECIALITEM_BACKSTAGE_PASS_PREFIX))
-                        {
-                            if (currentItem.SellIn < 11)
-                            {
-                                if (currentItem.Quality < MAX_NON_LEGENDARY_QUALITY)
-                                {
-                                    currentItem.Quality = currentItem.Quality + 1;
-                                }
-                            }
-
-                            if (currentItem.SellIn < 6)
-                            {
-                                if (currentItem.Quality < MAX_NON_LEGENDARY_QUALITY)
-                                {
-                                    currentItem.Quality = currentItem.Quality + 1;
-                                }
-                            }
-                        }
+                        currentItem.Quality = currentItem.SellIn > 0 ? currentItem.Quality -= 2 : currentItem.Quality -= 4;
                     }
                 }
-            }
-
-            if (currentItem.Quality != LEGENDARY_QUALITY)
-            {
-                currentItem.SellIn = currentItem.SellIn - 1;
-            }
-
-            if (currentItem.SellIn < 0)
-            {
-                if (!currentItem.Name.StartsWith(SPECIALITEM_AGED_ITEM_PREFIX))
-                {
-                    if (!currentItem.Name.StartsWith(SPECIALITEM_BACKSTAGE_PASS_PREFIX))
-                    {
-                        if (currentItem.Quality > MIN_NON_LEGENDARY_QUALITY)
-                        {
-                            if (currentItem.Quality != LEGENDARY_QUALITY)
-                            {
-                                if (currentItem.Name.StartsWith(SPECIALITEM_CONJURED_ITEM_NAME_PREFIX))
-                                {
-                                    currentItem.Quality -= 4;
-                                }
-                                else
-                                {
-                                    currentItem.Quality = currentItem.Quality - 1;
-                                }
-                            }
-                        }
-                    }
-                    else
-                    {
-                        currentItem.Quality = currentItem.Quality - currentItem.Quality;
-                    }
-                }
+                // else normal item
                 else
                 {
-                    if (currentItem.Quality < MAX_NON_LEGENDARY_QUALITY)
-                    {
-                        currentItem.Quality = currentItem.Quality + 1;
-                    }
+                    currentItem.Quality = currentItem.SellIn >= 0 ? currentItem.Quality -= 1 : currentItem.Quality -=2;
                 }
-            }
 
-            if (currentItem.Quality != LEGENDARY_QUALITY && currentItem.Quality > MAX_NON_LEGENDARY_QUALITY)
-            {
-                currentItem.Quality = MAX_NON_LEGENDARY_QUALITY;
+                // ensure quality is never greater than 50
+                if (currentItem.Quality > 50)
+                {
+                    // just set quality to 50
+                    currentItem.Quality = 50;
+                }
+
+                // ensure quality is never less than 0
+                if (currentItem.Quality < 0)
+                {
+                    // just set quality to 0
+                    currentItem.Quality = 0;
+                }
             }
         }
     }
