5e8249df47dd696dbce0f46a85853c1b4c2a761a
Chris Nightingale <wooffet@gmail.com>
2025-02-05 14:06:24 +0000

    Add test UpdateQuality_Item_QualityShouldNotBeNegative

--- csharp.NUnit/GildedRoseTests/GildedRoseTest.cs
+++ csharp.NUnit/GildedRoseTests/GildedRoseTest.cs
@@ -34,4 +34,14 @@ public class GildedRoseTest
         Assert.That(items[0].SellIn, Is.EqualTo(-1));
         Assert.That(items[0].Quality, Is.EqualTo(3));
     }
+
+    [Test]
+    public void UpdateQuality_Item_QualityShouldNotBeNegative()
+    {
+        var items = new List<Item> { new Item { Name = "foo", SellIn = -2, Quality = 1 } };
+        var app = new GildedRose(items);
+        app.UpdateQuality();
+        Assert.That(items[0].SellIn, Is.EqualTo(-3));
+        Assert.That(items[0].Quality, Is.EqualTo(0));
+    }
 }
